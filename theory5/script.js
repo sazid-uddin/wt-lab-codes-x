6dacf0209da4a3a845cc26840850f1527f832840
Md Sazid Uddin <sazid.uddin@aiub.edu>
2026-03-02 14:49:57 +0600

    JS Intro

---
+++ theory5/script.js
@@ -0,0 +1,38 @@
+// int a; strongly typed language
+
+// JS primitive data types
+var a = 2.5; // type: number
+var b = "Hello"; // type: string
+var b = 'Hello'; // type: string
+var str = "AIUB's policy";
+var str2 = 'He says "Hello"';
+var str3 = 'He say\'s "Hello"'; // escape character
+console.log(str3.length);
+console.log(str3);
+
+var bigNum = 123456789012345;
+var c = false; // type: boolean
+var d; // type: undefined and value is undefined
+
+console.log("NAN: " + isNaN("Hello"));
+
+console.log("Hello world");
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+
+var student = {
+	name: "John",
+	id: 123,
+	dob: "01-01-2000"
+}
+
+console.log(student.name);
+
+// var true = 
